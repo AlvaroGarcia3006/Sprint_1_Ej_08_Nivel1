a6318adb34ff0978822bf13c042958de785caf28
AlvaroGarcia3006 <113432141+AlvaroGarcia3006@users.noreply.github.com>
2022-10-11 11:20:35 +0200

    final e4

---
+++ src/main/java/nivel1_ej04/Main.java
@@ -0,0 +1,15 @@
+package nivel1_ej04;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio",
+                "Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+        list.forEach(Main::printMonths);
+    }
+    public static void printMonths(String month) {
+        System.out.println(month);
+    }
+}
