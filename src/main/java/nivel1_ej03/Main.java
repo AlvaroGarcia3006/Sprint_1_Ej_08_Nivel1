1ac27133c23b8a9708ec46f7c3135691622c199b
AlvaroGarcia3006 <113432141+AlvaroGarcia3006@users.noreply.github.com>
2022-10-11 11:19:29 +0200

    final e3

--- src/main/java/nivel1_ej03/Main.java
+++ src/main/java/nivel1_ej03/Main.java
@@ -7,9 +7,6 @@ public class Main {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio",
                 "Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-        list.forEach(Main::printMonths);
-    }
-    public static void printMonths(String month) {
-        System.out.println(month);
+        list.forEach(l-> System.out.println(l));
     }
 }
