28a05d90fc1dc67ac255b34ee2e556ad8315e688
AlvaroGarcia3006 <113432141+AlvaroGarcia3006@users.noreply.github.com>
2022-10-12 11:08:17 +0200

    final e6

---
+++ src/main/java/nivel1_ej06/Main.java
@@ -0,0 +1,15 @@
+package nivel1_ej06;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Abelardo","Febrero","Mayo","288","1","262661","Julio");
+        List<String> sortedList = list.stream().sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
+        System.out.println(list);
+        System.out.println(sortedList);
+    }
+}
